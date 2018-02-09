a901db8a754a9c6abbf8025b7ff2f0c21bdc8385
Kevin Durbin <kevindurb@gmail.com>
2018-02-09 07:57:10 -0700

    update react-toolbox

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import render from '@kevindurb/react-toolbox/render';
+import registerServiceWorker from '@kevindurb/react-toolbox/registerServiceWorker';
 import App from './src/components/App';
 
 export default render(<App />);
+
+registerServiceWorker();
